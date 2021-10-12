e218f8cef6cf74270db51f1339fde7d293eb6b80
Doyun Kwon <caen4516@gmail.com>
2021-10-12 14:01:40 +0900

    Hello Window
    https://learnopengl.com/Getting-started/Hello-Window

---
+++ MyOpenGL/AppMain.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const int WINSIZE_X = 800;
+const int WINSIZE_Y = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WINSIZE_X, WINSIZE_Y, "Learn OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		system("pause");
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		system("pause");
+		return -1;
+	}
+
+	framebuffer_size_callback(window, WINSIZE_X, WINSIZE_Y);
+
+	// 게임루프?
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		// rendering commands here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		{
+			// 여기
+		}
+		glfwSwapBuffers(window); // 더블 버퍼링
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+// 유저가 화면 사이즈를 변경했을 때 호출되는 콜백
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+	// ESC를 눌렀을 때 윈도우가 닫히도록한다
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
