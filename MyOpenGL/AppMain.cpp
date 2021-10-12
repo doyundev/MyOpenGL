61c5bf9eae99539b295979d6cf65631d72d89284
Doyun Kwon <caen4516@gmail.com>
2021-10-12 18:08:02 +0900

    삼각형 그리기
    https://learnopengl.com/Getting-started/Hello-Triangle

--- MyOpenGL/AppMain.cpp
+++ MyOpenGL/AppMain.cpp
@@ -7,6 +7,9 @@ const int WINSIZE_Y = 600;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+unsigned int compileVertexShader();
+unsigned int compileFragmentShader();
+unsigned int prepareShaderProgram(unsigned int vertexShader, unsigned int fragmentShader);
 
 int main() {
 	glfwInit();
@@ -31,6 +34,46 @@ int main() {
 
 	framebuffer_size_callback(window, WINSIZE_X, WINSIZE_Y);
 
+	// 삼각형을 그려보자
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, 
+		-0.5f, 0.5f, 0.0f,
+		0.5f,  0.5f, 0.0f,
+		0.5f,  -0.5f, 0.0f
+	};
+
+	unsigned int indices[] = {
+		0,1,2,
+		0,2,3
+	};
+
+	// Vertex Buffer Object
+	unsigned int VBO; 
+	glGenBuffers(1, &VBO); // ID를 생성하는 것임
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Vertex Array Object
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// EBO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// 셰이더 준비
+	unsigned int vertexShader = compileVertexShader();
+	unsigned int fragmentShader = compileFragmentShader();
+	unsigned int shaderProgram = prepareShaderProgram(vertexShader, fragmentShader);
+
 	// 게임루프?
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
@@ -39,7 +82,13 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		{
-			// 여기
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+			// 주석을 풀면 와이어프레임으로 보인다.
+			//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		}
 		glfwSwapBuffers(window); // 더블 버퍼링
 		glfwPollEvents();
@@ -58,4 +107,47 @@ void processInput(GLFWwindow *window) {
 	// ESC를 눌렀을 때 윈도우가 닫히도록한다
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+}
+
+unsigned int prepareShaderProgram(unsigned int vertexShader, unsigned int fragmentShader) {
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// 링크하고 나면 사용하지 않으므로 제거한다
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+}
+
+unsigned int compileVertexShader() {
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	return vertexShader;
+}
+
+unsigned int compileFragmentShader() {
+	const char *fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main(){\n"
+		"	FragColor = vec4(0.3f,0.85f,1.0f,1.0f);\n"
+		"}\0";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	return fragmentShader;
 }
